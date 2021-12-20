1a20aa1e2b1b9d060102ebeee9ecd580c32846db
changyonghui <406367076@qq.com>
2021-12-20 16:13:04 +0800

    2021.12.20 新增切换城市和手动更新天气的功能

--- app/src/main/java/com/cyh/sunnyweather/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/cyh/sunnyweather/ui/weather/WeatherActivity.kt
@@ -1,5 +1,6 @@
 package com.cyh.sunnyweather.ui.weather
 
+import android.content.Context
 import android.graphics.Color
 import android.icu.text.SimpleDateFormat
 import android.os.Build
@@ -8,12 +9,16 @@ import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.cyh.sunnyweather.R
+import com.cyh.sunnyweather.logic.Repository.refreshWeather
 import com.cyh.sunnyweather.logic.model.Weather
 import com.cyh.sunnyweather.logic.model.getSky
 import kotlinx.android.synthetic.main.activity_weather.*
@@ -50,11 +55,39 @@ class WeatherActivity : AppCompatActivity() {
                 Toast.makeText(this, "无法成功获取天气信息", Toast.LENGTH_SHORT).show()
                 result.exceptionOrNull()?.printStackTrace()
             }
+            swipeRefresh.isRefreshing = false
         })
-        Log.e("TAG", "onCreate: "+ viewModel.locationLng)
-        Log.e("TAG", "onCreate: "+ viewModel.locationLat)
-        viewModel.refreshWeather(viewModel.locationLng, viewModel.locationLat)
+        swipeRefresh.setColorSchemeResources(R.color.colorPrimary)
+        refreshWeather()
+        swipeRefresh.setOnRefreshListener {
+            refreshWeather()
+        }
+//        Log.e("TAG", "onCreate: "+ viewModel.locationLng)
+//        Log.e("TAG", "onCreate: "+ viewModel.locationLat)
+//        viewModel.refreshWeather(viewModel.locationLng, viewModel.locationLat)
+
+        navBtn.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        //隐藏输入法
+        drawerLayout.addDrawerListener(object : DrawerLayout.DrawerListener {
+            override fun onDrawerStateChanged(newState: Int) {}
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {}
+            override fun onDrawerOpened(drawerView: View) {}
+            override fun onDrawerClosed(drawerView: View) {
+                val manager = getSystemService(Context.INPUT_METHOD_SERVICE)
+                        as InputMethodManager
+                manager.hideSoftInputFromWindow(drawerView.windowToken,
+                    InputMethodManager.HIDE_NOT_ALWAYS)
+            }
+        })
     }
+
+    fun refreshWeather() {
+        viewModel.refreshWeather(viewModel.locationLng, viewModel.locationLat)
+        swipeRefresh.isRefreshing = true
+    }
+
     private fun showWeatherInfo(weather: Weather) {
         placeName.text = viewModel.placeName
         val realtime = weather.realtime
